d8c9ea73c929b3306abb46695e632cfa26db20d8
ppw0021 <ppw0021@autuni.ac.nz>
2025-06-06 15:12:12 +1200

    Added Model Code and Typst

---
+++ report.typ
@@ -0,0 +1,30 @@
+#set page("a4")
+
+#set page(
+  header: [
+    _ENSE803 Design Team Project_
+    #h(1fr)
+    Vending Machine Controller Verification
+  ],
+)
+
+#set page(
+  footer: context [
+    _Declan Ross, Anson Huang, Mahiir Hussain Shaik_
+    #h(1fr)
+    #counter(page).display(
+      "1 of 1",
+      both: true,
+    )
+  ],
+)
+
+= Design Team Project - Vending Machine Controller Verification 
+== ENSE803 - Formal Specification and Design
+=== Declan Ross (20108351) 
+=== Anson Huang (20120333)
+=== Mahiir Hussain Shaik (********)
+
+#pagebreak()
+
+= PRISM Model
---
+++ vendingMachineController.pm
@@ -0,0 +1,55 @@
+mdp
+
+module DrinkSelectionInterfaceComponent
+	//0 = none. 1 = Kiwi-Cola. 2 = Bolt Energy. 3 = Clear Water
+	selection: [0..3] init 0;
+
+	//Transitions for selecting drinks
+	[select_kiwi] selection=0 -> (selection'=1);
+	[select_bolt] selection=0 -> (selection'=2);
+	[select_water] selection=0 -> (selection'=3);
+
+	//Allow changing selection
+	[change_selection] selection>0 -> (selection'=0);
+endmodule
+
+module EFPOSPaymentComponent
+	//0 = Not paid. 1 = Paid. 2 = Incorrect Pin
+	payment_status: [0..2] init 0;
+    
+	//Transitions for payment
+	[enter_pin_correct] payment_status=0 -> (payment_status'=1);
+	[enter_pin_incorrect] payment_status=0 -> (payment_status'=2);
+
+	//Reset after incorrect pin
+	[reset_transaction] payment_status=2 -> (payment_status'=0);
+endmodule
+
+module DrinkDispenserComponent
+	//Drink levels, for this example we start with 5 each
+	kiwi_stock: [0..5] init 5;
+	bolt_stock: [0..5] init 5;
+	water_stock: [0..5] init 5;
+
+	//Errors, here 0 is false, and 1 is true
+	error: [0..1] init 0;
+	maintenance: [0..1] init 0;
+
+	//Dispense transistions
+	//Dispense kiwi if the payment status is 1, selection is 1, and kiwi_stock is greater than 0
+	[dispense_kiwi] payment_status=1 & selection=1 & kiwi_stock>0 -> (kiwi_stock'=kiwi_stock-1);
+
+	//Dispense bolt if the payment status is 1, selection is 2, and bolt_stock is greater than 0
+	[dispense_bolt] payment_status=1 & selection=2 & bolt_stock>0 -> (bolt_stock'=bolt_stock-1);
+
+	//Dispense water if the payment status is 1, selection is 3, and water_stock is greater than 0
+	[dispense_water] payment_status=1 & selection=3 & water_stock>0 -> (water_stock'=water_stock-1);
+
+	//Error transistion
+	[error_event] error=1 -> (maintenance'=1);
+
+	//Maintenance if any drink runs out
+	[check_stock] (kiwi_stock=0 | bolt_stock=0 | water_stock=0) -> (maintenance'=1);
+endmodule
+
+
