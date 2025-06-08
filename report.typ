7d0b0331dc9fcda08d6b2f9dabc491d6cb1c435b
Declan <ppw0021@autuni.ac.nz>
2025-06-09 01:08:33 +1200

    Fixed the structure

--- report.typ
+++ report.typ
@@ -45,80 +45,11 @@ Mahiir Hussain Shaik (21154501)\
 == PRISM Model
 Here is our PRISM model for the project, it was created and initially written in VSCode, then tested in the PRISM model checker. 
 ```js
-mdp
 
-//Constants for drink types and stock
-const int none = 0;
-const int kiwi = 1;
-const int bolt = 2;
-const int water = 3;
-const int max_stock = 3;
-
-module DrinkSelection
-    state: [0..3] init none; //0=none, 1=Kiwi-Cola, 2=Bolt Energy, 3=Clear Water
-
-    //Select drink if stock available and not in maintenance mode
-    [select_kiwi] state=none & kiwi_stock>0 & !maintenance -> (state'=kiwi);
-    [select_bolt] state=none & bolt_stock>0 & !maintenance -> (state'=bolt);
-    [select_water] state=none & water_stock>0 & !maintenance -> (state'=water);
-
-    //Change selection before payment
-    [change_selection] state>none & !maintenance & !pay -> (state'=none);
-    
-    //Synchronize with payment and reset after dispensing
-    [pay] state>none & pay & !maintenance -> (state'=none);
-    
-    //Reset on incorrect PIN
-    [reset] state>none & payment_error & !maintenance -> (state'=none);
-endmodule
-
-module EFPOSPayment
-    pay: bool init false; //True when payment is successful
-    payment_error: bool init false; //True when incorrect PIN entered
-    
-    //Start payment after selection
-    [start_payment] state>none & !pay & !payment_error & !maintenance -> (pay'=true);
-    
-    //Incorrect PIN
-    [wrong_pin] state>none & !pay & !payment_error & !maintenance -> (payment_error'=true);
-    
-    //Reset after incorrect PIN
-    [reset] payment_error & !maintenance -> (payment_error'=false) & (pay'=false);
-    
-    //Reset after dispensing
-    [dispense] pay & !maintenance -> (pay'=false);
-endmodule
 ```
 #pagebreak()
 ```js
-module Dispenser
-    kiwi_stock: [0..max_stock] init max_stock;
-    bolt_stock: [0..max_stock] init max_stock;
-    water_stock: [0..max_stock] init max_stock;
-    error: bool init false;
-    maintenance: bool init false;
-    dispense: bool init false;
-    
-    //Dispense drink if paid and stock available
-    [dispense] pay & state=kiwi & kiwi_stock>0 & !maintenance -> (kiwi_stock'=kiwi_stock-1) & (dispense'=true);
-    [dispense] pay & state=bolt & bolt_stock>0 & !maintenance -> (bolt_stock'=bolt_stock-1) & (dispense'=true);
-    [dispense] pay & state=water & water_stock>0 & !maintenance -> (water_stock'=water_stock-1) & (dispense'=true);
-    
-    //Reset dispense flag
-    [reset_dispense] dispense & !maintenance -> (dispense'=false);
-    
-    //Simulate error event
-    [error_event] !error & !maintenance -> 0.99:(error'=false) + 0.01:(error'=true);
-    
-	//Error event
-	[error_event] error & !maintenance -> (maintenance'=true);
-    
-    //Maintenance mode when all drinks are empty
-    [check_stock] kiwi_stock=0 & bolt_stock=0 & water_stock=0 & !maintenance -> (maintenance'=true);
-    
-    //Stay in maintenance mode
-    [] maintenance -> (maintenance'=true);
-endmodule
+
 ```
 //#pagebreak()
 ==== Design Decisions
@@ -127,8 +58,6 @@ endmodule
 2. _Clear and Concise Variable Naming._ Descriptive and consistent variable names such as _state_, _kiwi_stock_, and _pay_ are used throughout the model. This improves code clarity and helps users understand the model's behavior at a glance.
 
 3. _Well-Defined Transitions._ Transitions are defined with precise guard conditions to ensure that actions only occur under valid circumstances. For instance, the dispense transition only triggers when payment has been successfully made and a valid drink selection has been made.
-
-4. _Error Modeling with Probabilistic Transitions._ We introduced a probabilistic transition for simulating internal machine faults ([error_event] with 0.01 probability). This models real-world uncertainty and allows for analysis of system reliability using probabilistic model checking.
 
 #pagebreak()
 == Scenarios
--- vendingMachineController.pm
+++ vendingMachineController.pm
@@ -7,22 +7,38 @@ const int bolt = 2;
 const int water = 3;
 const int max_stock = 3;
 
+const int drinkSelectionModule = 0;
+const int efposPaymentModule = 1;
+const int dispenserModule = 2;
+
+const int readyToDispense = 0;
+const int dispensing = 1;
+const int doneDispensing = 2;
+
 module DrinkSelection
-    state: [0..3] init none; //0=none, 1=Kiwi-Cola, 2=Bolt Energy, 3=Clear Water
+    moduleState: [0..2] init drinkSelectionModule;
+    state: [0..3] init none; // 0=none, 1=Kiwi-Cola, 2=Bolt Energy, 3=Clear Water
+    selected_state: [0..3] init none;
+    // Select drink if stock available and not in maintenance mode
+    [select_kiwi] moduleState=drinkSelectionModule & state=none & kiwi_stock>0 & !maintenance -> (state'=kiwi);
+    [select_bolt] moduleState=drinkSelectionModule & state=none & bolt_stock>0 & !maintenance -> (state'=bolt);
+    [select_water] moduleState=drinkSelectionModule & state=none & water_stock>0 & !maintenance -> (state'=water);
 
-    //Select drink if stock available and not in maintenance mode
-    [select_kiwi] state=none & kiwi_stock>0 & !maintenance -> (state'=kiwi);
-    [select_bolt] state=none & bolt_stock>0 & !maintenance -> (state'=bolt);
-    [select_water] state=none & water_stock>0 & !maintenance -> (state'=water);
+    
+    // Change selection before payment
+    [change_selection] moduleState=drinkSelectionModule & state>none & !maintenance & !pay -> (state'=none);
 
-    //Change selection before payment
-    [change_selection] state>none & !maintenance & !pay -> (state'=none);
-    
-    //Synchronize with payment and reset after dispensing
-    [pay] state>none & pay & !maintenance -> (state'=none);
-    
-    //Reset on incorrect PIN
-    [reset] state>none & payment_error & !maintenance -> (state'=none);
+    // Synchronize with payment and reset after dispensing
+    [pay] moduleState=drinkSelectionModule & state>none & pay=false & !maintenance -> (selected_state'=state) & (state'=none) & (moduleState'=efposPaymentModule);
+
+    // Reset on incorrect PIN
+    [reset] moduleState=drinkSelectionModule & state>none & payment_error & !maintenance -> (state'=none);
+
+    //Transisiton to dispenser
+    [transistion_to_dispense] moduleState=efposPaymentModule & pay -> (moduleState'=dispenserModule);
+
+    //Transistion back to start
+    [transistion_to_start] moduleState=doneDispensing & pay & dispense=doneDispensing -> (moduleState'=drinkSelectionModule) & (state'=none) & (selected_state'=none);
 endmodule
 
 module EFPOSPayment
@@ -30,16 +46,16 @@ module EFPOSPayment
     payment_error: bool init false; //True when incorrect PIN entered
     
     //Start payment after selection
-    [start_payment] state>none & !pay & !payment_error & !maintenance -> (pay'=true);
+    [start_payment] moduleState=efposPaymentModule & selected_state>none & pay=false & payment_error=false & maintenance=false -> (pay'=true);
     
     //Incorrect PIN
-    [wrong_pin] state>none & !pay & !payment_error & !maintenance -> (payment_error'=true);
+    [wrong_pin] moduleState=efposPaymentModule & state>none & pay=false & payment_error=false & maintenance=false -> (payment_error'=true);
     
     //Reset after incorrect PIN
-    [reset] payment_error & !maintenance -> (payment_error'=false) & (pay'=false);
+    [reset] moduleState=efposPaymentModule & payment_error & maintenance=false -> (payment_error'=false) & (pay'=false);
     
     //Reset after dispensing
-    [dispense] pay & !maintenance -> (pay'=false);
+    [reset_pay] moduleState=drinkSelectionModule & !maintenance & dispense=readyToDispense & pay -> (pay'=false);
 endmodule
 
 module Dispenser
@@ -48,21 +64,18 @@ module Dispenser
     water_stock: [0..max_stock] init max_stock;
     error: bool init false;
     maintenance: bool init false;
-    dispense: bool init false;
+    dispense: [0..2] init readyToDispense;
     
+    [reset_dispense] moduleState=drinkSelectionModule & dispense=doneDispensing -> (dispense'=readyToDispense);
+
     //Dispense drink if paid and stock available
-    [dispense] pay & state=kiwi & kiwi_stock>0 & !maintenance -> (kiwi_stock'=kiwi_stock-1) & (dispense'=true);
-    [dispense] pay & state=bolt & bolt_stock>0 & !maintenance -> (bolt_stock'=bolt_stock-1) & (dispense'=true);
-    [dispense] pay & state=water & water_stock>0 & !maintenance -> (water_stock'=water_stock-1) & (dispense'=true);
+    [dispense_kiwi] moduleState=dispenserModule & pay & selected_state=kiwi & kiwi_stock>0 & dispense=readyToDispense & !maintenance -> (kiwi_stock'=kiwi_stock-1) & (dispense'=dispensing);
+    [dispense_bolt] moduleState=dispenserModule & pay & selected_state=bolt & bolt_stock>0 & dispense=readyToDispense & !maintenance -> (bolt_stock'=bolt_stock-1) & (dispense'=dispensing);
+    [dispense_water] moduleState=dispenserModule & pay & selected_state=water & water_stock>0 & dispense=readyToDispense & !maintenance -> (water_stock'=water_stock-1) & (dispense'=dispensing);
     
     //Reset dispense flag
-    [reset_dispense] dispense & !maintenance -> (dispense'=false);
-    
-    //Simulate error event
-    [error_event] !error & !maintenance -> 0.99:(error'=false) + 0.01:(error'=true);
-    
-	//Error event
-	[error_event] error & !maintenance -> (maintenance'=true);
+    //[reset_dispense] dispense & !maintenance -> (dispense'=false);
+    [set_dispense_to_done] dispense=dispensing -> (dispense'=doneDispensing);
     
     //Maintenance mode when all drinks are empty
     [check_stock] kiwi_stock=0 & bolt_stock=0 & water_stock=0 & !maintenance -> (maintenance'=true);
